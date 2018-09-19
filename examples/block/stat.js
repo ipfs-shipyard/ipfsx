c26d681b4ee7ee877e782679b9db44655484385e
Alan Shaw <alan.shaw@protocol.ai>
2018-09-19 09:16:23 +0100

    docs: add block.stat example
    License: MIT
    Signed-off-by: Alan Shaw <alan.shaw@protocol.ai>

---
+++ examples/block/stat.js
@@ -0,0 +1,14 @@
+const Os = require('os')
+const Path = require('path')
+const Ipfs = require('ipfs')
+const ipfsx = require('../../')
+
+;(async () => {
+  const node = await ipfsx(new Ipfs({ repo: Path.join(Os.tmpdir(), `${Date.now()}`) }))
+  const data = Buffer.from('hello world')
+  const block = await node.block.put(data).first()
+  const stats = await node.block.stat(block.cid)
+
+  console.log(block.cid.toBaseEncodedString(), stats)
+  // zb2rhj7crUKTQYRGCRATFaQ6YFLTde2YzdqbbhAASkL9uRDXn { size: 11 }
+})()
