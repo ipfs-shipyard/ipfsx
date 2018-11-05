dcd92f98fecd37ee29418771619b6bf81dc61e47
Alan Shaw <alan.shaw@protocol.ai>
2018-11-05 16:03:53 +0000

    tests: add dag.put tests
    License: MIT
    Signed-off-by: Alan Shaw <alan.shaw@protocol.ai>

--- test/dag.test.js
+++ test/dag.test.js
@@ -2,7 +2,7 @@ const test = require('ava')
 const { randomBytes } = require('crypto')
 const CID = require('cids')
 const Multihash = require('multihashes')
-const { randomInteger } = require('./helpers/random')
+const { randomInteger, randomArray } = require('./helpers/random')
 const ipfsx = require('./helpers/ipfsx')
 const { sha2mh } = require('./helpers/multihash')
 
@@ -14,6 +14,53 @@ test('should put a buffer', async t => {
   const data = randomBytes(randomInteger(1, 256))
   const cid = await node.dag.put(data).first()
   t.true(CID.isCID(cid))
+  t.is(cid.version, 1)
+  t.is(cid.codec, 'raw')
+})
+
+test('should put from iterator of buffer', async t => {
+  const { node } = t.context
+
+  const data = randomArray(1, 100, () => randomBytes(randomInteger(1, 64)))
+  let count = 0
+
+  for await (const cid of node.dag.put(data)) {
+    t.is(cid.version, 1)
+    t.is(cid.codec, 'raw')
+    count++
+  }
+
+  t.is(count, data.length)
+})
+
+test('should put from async iterable of buffer', async t => {
+  const { node } = t.context
+
+  const data = randomArray(1, 100, () => randomBytes(randomInteger(1, 64)))
+
+  const it = {
+    [Symbol.asyncIterator] () {
+      let i = 0
+      return {
+        async next () {
+          if (i === data.length) return { done: true }
+          const value = await new Promise((resolve, reject) => {
+            setTimeout(() => resolve(data[i++]), randomInteger(1, 10))
+          })
+          return { value, done: false }
+        }
+      }
+    }
+  }
+  let count = 0
+
+  for await (const cid of node.dag.put(it)) {
+    t.is(cid.version, 1)
+    t.is(cid.codec, 'raw')
+    count++
+  }
+
+  t.is(count, data.length)
 })
 
 test('should throw for missing IPLD format', async t => {
